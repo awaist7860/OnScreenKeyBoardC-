7be0a8c8944567aff41309e1d15dbe59148ef305
Awais Tasleem <48568803+awaist7860@users.noreply.github.com>
2022-09-25 12:37:36 +0100

    Basic Script Created

---
+++ OnScreenKeyboard/OnScreenKeyboard/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace OnScreenKeyboard
+{
+    public partial class Form1 : Form
+    {
+
+        protected override CreateParams CreateParams
+        {
+            get
+            {
+                CreateParams param = base.CreateParams;
+                    param.ExStyle |= 0x08000000;
+                    return param;
+            }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //SendKeys.Send("A");
+
+            messageSend();
+
+
+
+        }
+
+        public void messageSend()
+        {
+            int i = 1;
+
+            while(i < 10)
+            {
+                SendKeys.Send("Y");
+                SendKeys.Send("O");
+                SendKeys.Send(" ");
+                SendKeys.Send("C");
+                SendKeys.Send("a");
+                SendKeys.Send("l");
+                SendKeys.Send("l");
+                SendKeys.Send(" ");
+                SendKeys.Send("M");
+                SendKeys.Send("e");
+                SendKeys.Send("{ENTER}");
+                i++;
+                Thread.Sleep(250);
+            }
+        }
+    }
+}
